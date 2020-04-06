f193d8f3372f2ad1b2a6f43d0224425efe4fd76e
Lucas Costa <lucascsfaria@gmail.com>
2020-04-06 15:53:45 -0300

    Adicionando validações

--- start/routes.js
+++ start/routes.js
@@ -2,11 +2,11 @@
 
 const Route = use('Route')
  
-Route.post('users', 'USerController.store')
-Route.post('sessions', 'SessionController.store')
+Route.post('users', 'USerController.store').validator('User')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('passwords', 'ForgotPasswordController.store')
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
+Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
 
 Route.get('/files/:id', 'FileController.show')
 
@@ -14,7 +14,27 @@ Route.group(() => {
 
     Route.post('/files', 'FileController.store')
     
-    Route.resource('projects', 'ProjectController').apiOnly()
-    Route.resource('projects.tasks', 'TaskController').apiOnly()
+    Route.resource('projects', 'ProjectController')
+        .apiOnly()
+        .validator(new Map(
+            [
+                [
+                    ['projects.store'], 
+                    ['Project']
+                ]
+            ]
+        ))
+
+
+    Route.resource('projects.tasks', 'TaskController')
+        .apiOnly()
+        .validator(new Map(
+            [
+                [
+                    ['projects.tasks.store'], 
+                    ['Task']
+                ]
+            ]
+        ))
 
 }).middleware(['auth'])
