3a35b34235ee3681da67cedd2022e062aa566839
Lucas Costa <lucascsfaria@gmail.com>
2020-04-12 21:12:24 -0300

    Criando fila de emails para tarefas com o Redis

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
 
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 const TaskHook = exports = module.exports = {}
 
 TaskHook.sendNewTaskEmail = async taskInstance => {
@@ -16,20 +16,5 @@ TaskHook.sendNewTaskEmail = async taskInstance => {
 
     const { title } = taskInstance
 
-    await Mail.send(
-        ['emails.new_task'],
-        { username, title, hasAttachment: !!file},
-        message => {
-            message
-                .to(email)
-                .from('lucasbd26@gmail.com', 'Lucas | Curso Adonis')
-                .subject('Nova tarefa para você!')
-
-            if (file){
-                message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-                    filename: file.name
-                })
-            }
-        }
-    )
+   Kue.dispatch(Job.key, { email, username, file, title }, { attempts: 3 })
 }
